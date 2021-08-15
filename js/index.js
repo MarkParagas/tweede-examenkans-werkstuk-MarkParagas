6efa2a272ebb841bf31cdbc469c1ee0c9b8b5341
mark paragas <mark.paragas@student.ehb.be>
2021-08-15 14:51:48 +0200

    Functie laten defin en weergeven inner html

--- js/index.js
+++ js/index.js
@@ -1,42 +1,48 @@
 "use strict";
 
-function getNews() {
-    console.log('Get the news (testing)')
-    let test = '';
+let html = '';
+
+window.onload = () => {
     fetch('https://thecrew.cc/news/read.php')
         .then(response => response.json())
         .then(data => {
             console.log('Getting data');
             console.log(data);
-            test = data.news;
-        
+
+            data.news.forEach(news => {
+
+                html += `
+            <article>
+                <div class="image_wrapper">
+                    <img src="${news.imageURI}">
+                </div>
+                <div class="article_content_wrapper">
+                        <div class="title">
+                            <div class="info">
+                                <div class="datum"> Datum: ${news.publicationDate}</div>
+                                <div class="likes"> Likes: ${news.likes}</div>
+                            </div>
+                            <h1>${news.title}</h1>
+                        </div>
+                    <div class="content">
+                      ${news.content}
+                    </div>
+                </div>
+            </article>
+            `;
+
+            })
+
+            document.getElementById("container").innerHTML = html;
+
         });
-        renderNews(test); //insertNews(test);
 }
 
-function renderNews(newsartikel) {
-    document.getElementById("container").innerHTML = "";
-    newsartikel.forEach(news => {
-        let html = `
-        <article>
-        <div class="image_wrapper">
-          <img
-            src="${news[i].imageURI}">
-        </div>
-        <div class="article_content_wrapper">
-          <div>
-            <h3>${news[i].title}</h3> <!-- Title -->
-          </div>
-          <div>
-          ${news[i].content}
-          </div>
-          <div class="likes"> likes: ${news[i].likes}</div>
-          <div class="datum"> datum: ${news[i].publicationDate}</div>
-        </div>
-      </article>
-        `;    document.getElementById("container").insertAdjacentHTML("beforeend", html);
-    });
-}
+/* 
 
-getNews();
+Vorige code waren niet alles in bepaalde functie worden gedefineerd
 
+1) Api ophalen
+2) Alles laten weergeven inner html
+
+*/
