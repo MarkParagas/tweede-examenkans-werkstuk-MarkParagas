218ca9c5c1b323e6c448212625a0360e73e97447
mark paragas <mark.paragas@student.ehb.be>
2021-08-15 16:47:37 +0200

    Error met search value (String.slice)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let html = '';
+let search = document.getElementById("keyName");
 
 window.onload = () => {
     fetch('https://thecrew.cc/news/read.php')
@@ -8,9 +9,13 @@ window.onload = () => {
         .then(data => {
             console.log('Getting data');
             console.log(data);
-
             data.news.forEach(news => {
 
+            if(search.value == news.title.search(search.value)){
+                valueNumber = string.slice(); // Error in search value
+            } else {
+                return html= `Geen resultaat`;
+            }
                 html += `
             <article>
                 <div class="image_wrapper">
@@ -40,9 +45,16 @@ window.onload = () => {
 
 /* 
 
+Error:
 Vorige code waren niet alles in bepaalde functie worden gedefineerd
 
+Succeed:
 1) Api ophalen
 2) Alles laten weergeven inner html
+3) Zoekbalk toevoegen en 
+
+progress:
+value opslaan
+
 
 */
